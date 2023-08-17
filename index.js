845290d15951ec1ccca92a6df6ff61fc5403b89d
Souradeep <souradeepghosh00@gmail.com>
2023-08-17 13:48:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app=express();
+const port=3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended : true }));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+
+
+const apikey='ec0a35e68dmsh04453053bfbc39fp124670jsn177fbe8632aa'; 
+const apihost='imdb8.p.rapidapi.com';
+
+
+app.post("/", async (req,res)=> {
+    let title=req.body.title;
+    try {
+        const response= await axios.get("https://imdb8.p.rapidapi.com/title/find",{
+            params: {
+                q : title,
+                limit : 15
+            }, 
+            headers :{
+                'X-RapidAPI-Key': apikey,
+                'X-RapidAPI-Host' : apihost
+
+            }
+        });
+        console.log("hello ji kaise ho saare");
+        //console.log(response.data.results);
+        const result=response.data.results;
+        let movie;
+        for(let i=0;i<result.length;i++)
+        {
+            if(result[i].titleType)
+            {
+                if(result[i].titleType=='movie')
+                {
+                    movie=result[i];
+                    break;
+                }
+            }
+        }
+        console.log(movie);
+        let mvid=movie.id.slice(7,-1);
+        const response1= await axios.get("https://imdb8.p.rapidapi.com/title/get-ratings",{
+            params : {
+                tconst : mvid
+            },
+            headers :{
+                'X-RapidAPI-Key': apikey,
+                'X-RapidAPI-Host' : apihost
+
+            }
+       });
+       console.log(response1.data.rating);
+        res.render("index.ejs",{movie: movie,
+            rating: response1.data.rating
+        });
+    } catch(error) {
+        console.log(error.message);
+        res.render("index.ejs", {error : error.message});
+    }
+});
+
+app.listen(port, ()=> {
+    console.log(`server is running on port ${port}`);
+});
