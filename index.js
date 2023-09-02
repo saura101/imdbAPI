39765c1a137ae15d527c876700bb3896a6f2161f
Souradeep <souradeepghosh00@gmail.com>
2023-09-02 16:35:50 +0530

    added the genre

--- index.js
+++ index.js
@@ -10,87 +10,148 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended : true }));
 
+
+let movieByGenreDetails = [];
+let movieDetails = [];
+let seriesDetails = [];
+
 app.get("/", async(req,res) => {
 
-    try {
-        const response = await axios.get("https://imdb8.p.rapidapi.com/title/get-most-popular-movies", {
-            params: {
-                homeCountry: 'IN',
-                purchaseCountry: 'IN',
-                currentCountry: 'IN'
-              },
-              headers: {
-                'X-RapidAPI-Key': apikey,
-                'X-RapidAPI-Host': apihost
-              }
-        });
-
-        const response1 = await axios.get("https://imdb8.p.rapidapi.com/title/get-most-popular-tv-shows", {
-            params: {
-                homeCountry: 'IN',
-                purchaseCountry: 'IN',
-                currentCountry: 'IN'
-              },
-              headers: {
-                'X-RapidAPI-Key': apikey,
-                'X-RapidAPI-Host': apihost
-              }
-        });
-
-
-        //console.log(response.data);
-        //console.log(response1.data);
-        let mostPopularMovies = response.data;
-        let mostPopularSeries = response1.data;
-        let movieDetails = [];
-        let seriesDetails = [];
-        for(let i=0;i<3;i++) {
-            let mvid=mostPopularMovies[i].slice(7,-1);
-            console.log(mvid);
-            let sid = mostPopularSeries[i].slice(7,-1);
-            console.log(sid);
-            const response= await axios.get("https://imdb8.p.rapidapi.com/title/get-details",{
-                    params : {
-                        tconst : mvid
-                    },
-                    headers :{
-                        'X-RapidAPI-Key': apikey,
-                        'X-RapidAPI-Host' : apihost
-                    }
-                });
-
-            const movie = {
-                name : response.data.title,
-                year : response.data.year,
-                image : response.data.image.url
+    if(movieDetails.length === 0) {
+        try {
+            const response = await axios.get("https://imdb8.p.rapidapi.com/title/get-most-popular-movies", {
+                params: {
+                    homeCountry: 'IN',
+                    purchaseCountry: 'IN',
+                    currentCountry: 'IN'
+                  },
+                  headers: {
+                    'X-RapidAPI-Key': apikey,
+                    'X-RapidAPI-Host': apihost
+                  }
+            });
+    
+            const response1 = await axios.get("https://imdb8.p.rapidapi.com/title/get-most-popular-tv-shows", {
+                params: {
+                    homeCountry: 'IN',
+                    purchaseCountry: 'IN',
+                    currentCountry: 'IN'
+                  },
+                  headers: {
+                    'X-RapidAPI-Key': apikey,
+                    'X-RapidAPI-Host': apihost
+                  }
+            });
+    
+    
+            //console.log(response.data);
+            //console.log(response1.data);
+            let mostPopularMovies = response.data;
+            let mostPopularSeries = response1.data;
+            
+            for(let i=0;i<3;i++) {
+                let mvid=mostPopularMovies[i].slice(7,-1);
+                console.log(mvid);
+                let sid = mostPopularSeries[i].slice(7,-1);
+                console.log(sid);
+                const response= await axios.get("https://imdb8.p.rapidapi.com/title/get-details",{
+                        params : {
+                            tconst : mvid
+                        },
+                        headers :{
+                            'X-RapidAPI-Key': apikey,
+                            'X-RapidAPI-Host' : apihost
+                        }
+                    });
+    
+                const movie = {
+                    name : response.data.title,
+                    year : response.data.year,
+                    image : response.data.image.url
+                }
+                movieDetails.push(movie);
+    
+                const response1= await axios.get("https://imdb8.p.rapidapi.com/title/get-details",{
+                        params : {
+                            tconst : sid
+                        },
+                        headers :{
+                            'X-RapidAPI-Key': apikey,
+                            'X-RapidAPI-Host' : apihost
+                        }
+                    });
+    
+                const serie = {
+                    name : response1.data.title,
+                    year : response1.data.year,
+                    image : response1.data.image.url
+                }
+                seriesDetails.push(serie);
+    
             }
-            movieDetails.push(movie);
-
-            const response1= await axios.get("https://imdb8.p.rapidapi.com/title/get-details",{
-                    params : {
-                        tconst : sid
-                    },
-                    headers :{
-                        'X-RapidAPI-Key': apikey,
-                        'X-RapidAPI-Host' : apihost
-                    }
-                });
-
-            const serie = {
-                name : response1.data.title,
-                year : response1.data.year,
-                image : response1.data.image.url
-            }
-            seriesDetails.push(serie);
-
+        } catch (error) {
+            console.error(error);
         }
+    }
+    if(movieByGenreDetails.length === 0) {
+        console.log("first");
         res.render("home.ejs" , {
             movies : movieDetails,
             series : seriesDetails
         });
+    } else {
+        res.render("home.ejs" , {
+            movies : movieDetails,
+            series : seriesDetails,
+            genre : movieByGenreDetails
+        });
+    }
+
+});
+
+app.get("/genre/:name", async(req,res)=> {
+    movieByGenreDetails = [];
+    let genre = req.params.name;
+    console.log(genre);
+    try {
+        const response = await axios.get("https://imdb8.p.rapidapi.com/title/v2/get-popular-movies-by-genre", {
+            params: {
+                genre: genre,
+                limit: '5'
+              },
+              headers: {
+                'X-RapidAPI-Key': apikey,
+                'X-RapidAPI-Host': apihost
+              }
+        });
+        //console.log(response.data);
+        
+
+        for(let i=0;i<3;i++) {
+            let mvid=response.data[i].slice(7,-1);
+            console.log(mvid);
+            const response1= await axios.get("https://imdb8.p.rapidapi.com/title/get-details",{
+                params : {
+                    tconst : mvid
+                },
+                headers :{
+                    'X-RapidAPI-Key': apikey,
+                    'X-RapidAPI-Host' : apihost
+                }
+            });
+        
+                    const movie = {
+                        ge : genre,
+                        name : response1.data.title,
+                        year : response1.data.year,
+                        image : response1.data.image.url
+                    }
+                    movieByGenreDetails.push(movie);
+        }
     } catch (error) {
         console.error(error);
     }
+    res.redirect("/")
 });
 
 app.get("/search", (req,res) => {
