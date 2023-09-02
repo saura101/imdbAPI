6b1a5b32283a8e23bac0326be3b809f1c04ecf9a
Souradeep <souradeepghosh00@gmail.com>
2023-09-02 13:34:57 +0530

    configure port option

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 
 const app=express();
-const PORT = (process.env.PORT || 3000 );
+const PORT = (process.env.PORT ||  3000 );
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
